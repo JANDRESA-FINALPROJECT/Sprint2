1462a0669f866800bb7b1f6023da2f7273191588
JANDRESA <97019274+JANDRESA-FINALPROJECT@users.noreply.github.com>
2022-06-04 19:54:48 +0800

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,39 +1,4 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing text animation script
+    //text animation-typing for home section
     var typed = new Typed(".typing", {
         strings: ["Come and Discover!"],
         typeSpeed: 30,
@@ -45,66 +10,36 @@ $(document).ready(function(){
         loop: true
     });
 	
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
-});
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() { 
-  $('#slideshow > div:first')
-  .fadeOut(100)
-  .next()
-  .fadeIn(100)
-  .end()
-  .appendTo('#slideshow');
-}, 800);
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
+// photos control for procedure section
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides(index = n);
 }
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  let slide = document.getElementsByClassName("mySlides");
+  let dot = document.getElementsByClassName("scrollbar");
+  if (n > slide.length) {index = 1}
+  if (n < 1) {index = slide.length}
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < dot.length; i++) {
+    dot[i].className = dot[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slide[index-1].style.display = "block";
+  dot[index-1].className += " active";
 }
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navigation bar
+        if(this.scrollY > 10){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+
+	// for smooth transistion when clicking the items in menu 
+    $('.navbar .list li a').click(function(){
+        $('html').css("scrollBehavior", "smooth");
+    });
